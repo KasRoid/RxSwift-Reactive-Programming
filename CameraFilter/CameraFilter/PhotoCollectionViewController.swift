2e2cabb59f3435a51782d178dfb17e618d09f7da
KasRoid <KasRoid@gmail.com>
2021-01-06 08:02:41 +0900

    init: CameraFilter Basic UI Setup

---
+++ CameraFilter/CameraFilter/PhotoCollectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCollectionViewController.swift
+//  CameraFilter
+//
+//  Created by Kas Song on 1/5/21.
+//
+
+import UIKit
+
+class PhotoCollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ CameraFilter/CameraFilter/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Kas Song on 1/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
