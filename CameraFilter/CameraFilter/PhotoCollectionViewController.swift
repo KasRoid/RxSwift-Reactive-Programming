c8b80e5f67135664e20a8e34520e2a3ec4157d6f
KasRoid <KasRoid@gmail.com>
2021-01-09 15:25:03 +0900

    feat: Requesting Permission to Access Phto Library

--- CameraFilter/CameraFilter/PhotoCollectionViewController.swift
+++ CameraFilter/CameraFilter/PhotoCollectionViewController.swift
@@ -6,10 +6,31 @@
 //
 
 import UIKit
+import Photos
 
 class PhotoCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        populatePhotos()
+    }
+    
+    private func populatePhotos() {
+        PHPhotoLibrary.requestAuthorization { status in
+            switch status {
+            case .authorized:
+                break
+            case .denied:
+                break
+            case .limited:
+                break
+            case .notDetermined:
+                break
+            case .restricted:
+                break
+            @unknown default:
+                fatalError()
+            }
+        }
     }
 }
