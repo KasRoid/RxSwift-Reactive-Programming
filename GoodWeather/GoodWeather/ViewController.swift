215cc3cd1bb987165b9fc1362cccb6a090574a53
KasRoid <KasRoid@gmail.com>
2021-01-11 22:15:00 +0900

    UI: Building User Interface

---
+++ GoodWeather/GoodWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Kas Song on 1/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Properties
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var temperatureLbel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
