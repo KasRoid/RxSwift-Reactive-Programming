aab95c8d1ab144d278166bf80400f22f757324e6
KasRoid <KasRoid@gmail.com>
2021-01-12 21:52:52 +0900

    feat: Implementing URLRequest Model

---
+++ GoodWeather/GoodWeather/Extensions/URLRequest+Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  URLRequest+Extensions.swift
+//  GoodWeather
+//
+//  Created by Kas Song on 1/12/21.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+struct Resource<T> {
+    let url: URL
+}
+
+extension URLRequest {
+    static func load<T: Decodable>(resource: Resource<T>) -> Observable<T> {
+        return Observable.from([resource.url])
+            .flatMap { url -> Observable<Data> in
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.data(request: request)
+            }.map { data -> T in
+                return try JSONDecoder().decode(T.self, from: data)
+            }.asObservable()
+    }
+}
