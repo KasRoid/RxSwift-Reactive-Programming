d25003fb396b59834fc0ef3fac47940423d1c519
KasRoid <KasRoid@gmail.com>
2021-01-16 18:09:06 +0900

    feat: Populating and Displaying News in TableView

--- NewsAppMVVM/NewsAppMVVM/Controllers/NewsTableViewController.swift
+++ NewsAppMVVM/NewsAppMVVM/Controllers/NewsTableViewController.swift
@@ -12,6 +12,7 @@ class NewsTableViewController: UITableViewController {
     
     // MARK: - Properties
     private let disposeBag = DisposeBag()
+    private var articleListVM: ArticleListViewModel!
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -24,8 +25,30 @@ class NewsTableViewController: UITableViewController {
         let url = URL(string: "")!
         let resource = Resource<ArticleResponse>(url: url)
         URLRequest.load(resource: resource)
-            .subscribe(onNext: {
-                print($0)
+            .subscribe(onNext: { articleResponse in
+                let articles = articleResponse.articles
+                self.articleListVM = ArticleListViewModel(articles)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }).disposed(by: disposeBag)
+    }
+}
+
+extension NewsTableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.articlesVM.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else { fatalError() }
+        let articleVM = self.articleListVM.articleAt(indexPath.row)
+        articleVM.title.asDriver(onErrorJustReturn: "")
+            .drive(cell.titleLabel.rx.text)
+            .disposed(by: disposeBag)
+        articleVM.description.asDriver(onErrorJustReturn: "")
+            .drive(cell.descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        return cell
     }
 }
---
+++ NewsAppMVVM/NewsAppMVVM/Views/ArticleTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsAppMVVM
+//
+//  Created by Kas Song on 1/16/21.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
